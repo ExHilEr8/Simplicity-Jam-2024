a2e3e7c7118904c0175be58bd4aadc7297dc28c1
ExHilEr8 <justinkwietniowski@gmail.com>
2024-12-06 01:47:22 -0500

    Remove health_component.tscn

--- scenes/components/player_health_component.gd
+++ scenes/components/player_health_component.gd
@@ -1,11 +1,2 @@
-extends Node
+extends HealthComponent
 class_name PlayerHealthComponent
-
-@export_group("General")
-@export var health_max : int = 3
-
-@onready var health : int = health_max :
-	set(value):
-		health = clamp(value, 0, health_max)
-	get:
-		return health
